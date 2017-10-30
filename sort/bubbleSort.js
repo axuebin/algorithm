3529bd7c36b11f3f0c2174de92ad0bdc3f70d378
axuebin <xbmail2013@163.com>
2017-10-30 09:30:00 +0800

    Update bubbleSort

--- sort/bubbleSort.js
+++ sort/bubbleSort.js
@@ -6,7 +6,7 @@ function bubbleSort(arr) {
   const len = arr.length; // 声明一个len来存储数组的长度
   let temp = 0;
   for (let i = 0; i < len; i += 1) { // 外循环遍历数组
-    for (let j = 0; j < len - 1; j += 1) { // 内循环执行当前项和下一项进行比较
+    for (let j = 0 ; j < len - 1 ; j += 1) { // 内循环执行当前项和下一项进行比较
       if (arr[j] > arr[j + 1]) {  // 如果当前项比下一项大，则交换它们
         temp = arr[j];
         arr[j] = arr[j + 1];
@@ -17,12 +17,12 @@ function bubbleSort(arr) {
   return arr;
 }
 
-// 第二次循环时，i前面的数其实已经排好序了，就不要再次比较了，可以在内循环中减去已经跑过的轮数，就可以避免不必要的比较
+// 第二次循环时，后面其实已经排好序了，就不要再次比较了，可以在内循环中减去已经跑过的轮数，就可以避免不必要的比较
 function bubbleSort(arr) {
   const len = arr.length;
   let temp = 0;
   for (let i = 0; i < len; i += 1) {
-    for (let j = i + 1; j < len - 1 - i; j += 1) {
+    for (let j = 0 ; j < len - 1 - i ; j += 1) {
       if (arr[j] > arr[j + 1]) {
         temp = arr[j];
         arr[j] = arr[j + 1];
